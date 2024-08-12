16e7eda3635a5ff66537d1c5bd1d15f3fb7e605d
Aryan-Sood <aryansood12@gmail.com>
2024-08-12 22:27:26 +0530

    serialization

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,28 +1,36 @@
-# from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse, HttpResponse
 import json
 from django.forms.models import model_to_dict
 from products.models import Product
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from products.serializers import ProductSerializer
 
 
-@api_view(["GET","POST"])
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
 
-    model_data = Product.objects.all().order_by("?").first()
-    data = {}
+    # instance = Product.objects.all().order_by("?").first()
+    data = request.data
+    serializer = ProductSerializer(data = request.data)
+    if serializer.is_valid():
+        data = serializer.save()
+        print(data)
+        return Response(serializer.data)
 
-    if request.method != "POST":
-        return Response({"detail":"GET Not Allowed"},status=400)
+    # if request.method != "POST":
+    #     return Response({"detail":"GET Not Allowed"},status=400)
 
 
-    if model_data:
-        data = model_to_dict(model_data, fields=['id','title','price'])
+    # if instance:
+        # data = model_to_dict(model_data, fields=['id','title','price','sale_price'])
         # data['id'] = model_data.id
         # data['title'] = model_data.title
         # data['content'] = model_data.content
         # data['price'] = model_data.price
+        # data['sale_price'] = model_data.sale_price
         # print("data: "+ str(data))
-        return Response(data)
+        # data = ProductSerializer(instance).data
+   
         # json_data_str = json.dumps(data)
         # return HttpResponse(json_data_str, headers = {"content-type":"application/json"})
