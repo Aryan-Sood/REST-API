b3d9b020c1243bd95a967e2cda96139abf60ace1
Aryan-Sood <aryansood12@gmail.com>
2024-08-12 21:51:03 +0530

    integrate django rest view for api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,28 @@
-from django.http import JsonResponse
+# from django.http import JsonResponse, HttpResponse
 import json
+from django.forms.models import model_to_dict
+from products.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+
+@api_view(["GET","POST"])
 def api_home(request, *args, **kwargs):
-    body = request.body   # string of json data
+
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    print(data)
-    return JsonResponse(data)
+
+    if request.method != "POST":
+        return Response({"detail":"GET Not Allowed"},status=400)
+
+
+    if model_data:
+        data = model_to_dict(model_data, fields=['id','title','price'])
+        # data['id'] = model_data.id
+        # data['title'] = model_data.title
+        # data['content'] = model_data.content
+        # data['price'] = model_data.price
+        # print("data: "+ str(data))
+        return Response(data)
+        # json_data_str = json.dumps(data)
+        # return HttpResponse(json_data_str, headers = {"content-type":"application/json"})
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,5 +3,5 @@ import requests
 #endpoint = "https://httpbin.org"   # this gives a html file as text
 endpoint = "http://localhost:8000/api/"   # this gives a json response
 
-get_response = requests.get(endpoint, params={"abc":123}, json={"query":"Hello world"})
-print(get_response)
+get_response = requests.get(endpoint)
+print(get_response.json())
